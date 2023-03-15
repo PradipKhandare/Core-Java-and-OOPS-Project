477ffecd89959f559292a6c0ef0788617d0d137c
Pradip Khandare <121931206+PradipKhandare@users.noreply.github.com>
2023-03-15 16:36:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package SimpleGameCoreJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Player1 player = new Player1("Pradip", "sword", 100);
+
+//		System.out.println(player.getName());
+//		System.out.println(player.getHealth());
+//		System.out.println(player.getWeapon());
+
+		player.damageByGun1();
+		player.damageByGun1();
+		player.damageByGun2();
+		player.heal();
+
+	}
+
+}
